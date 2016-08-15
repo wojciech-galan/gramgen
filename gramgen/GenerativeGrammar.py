33777a2a07fbbe065eb7e4ae215cb1b3340fc445
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2016-08-15 08:21:06 +0200

    license - formatting changed, productions added

--- gramgen/GenerativeGrammar.py
+++ gramgen/GenerativeGrammar.py
@@ -1,24 +1,24 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-""" This file is a part of an library named gramgen
-    If you have any questions and/or comments, don't hesitate to
-    contact me by email wojciech.galan@gmail.com
-
-    Copyright (C) 2016  Wojciech Gałan
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>"""
+    # This file is a part of an library named gramgen
+    # If you have any questions and/or comments, don't hesitate to
+    # contact me by email wojciech.galan@gmail.com
+    #
+    # Copyright (C) 2016  Wojciech Gałan
+    #
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+    #
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+    #
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 
 import pdb
